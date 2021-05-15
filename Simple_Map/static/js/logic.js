a0756c453dd5fbbddde319dbdbb372b361671340
Onejledonlala <78067427+Onejledonlala@users.noreply.github.com>
2021-05-15 01:58:05 -0400

    setting up markers

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -4,13 +4,28 @@ console.log("working");
 // Create the map object with a center and zoom level.
 let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// //  Create the map object with a center and zoom level.
+// //    Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
 //     center: [
 //       40.7, -94.5
 //     ],
 //     zoom: 4
 //   });
+
+// //  Add a marker to the map for Los Angeles, California.
+// L.circle([34.0522, -118.2437], {
+//     radius: 100
+//  }).addTo(map);
+
+// //  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a Circle to the map
+L.circleMarker([34.0522, -118.2437], {
+    radius: 100,
+    color: 'black',
+    fillColor: '#ffffa1'
+}).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -22,6 +37,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
+// // We create the tile layer that will be the background of our map.
+// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 
 
 
