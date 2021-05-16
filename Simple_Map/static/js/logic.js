e40f3de87bfc7a5c320bd880a450f0a7d409308f
Onejledonlala <78067427+Onejledonlala@users.noreply.github.com>
2021-05-16 14:57:09 -0400

    adding geojson

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -1,8 +1,8 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// // Create the map object with a center and zoom level.
-// let map = L.map('mapid').setView([40.7, -94.5], 4);
+// Create the map object with a center and zoom level.
+let map = L.map('mapid').setView([37.5, -122.5], 10);
 
 // //    Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -20,8 +20,8 @@ console.log("working");
 // //  Add a marker to the map for Los Angeles, California.
 // let marker = L.marker([34.0522, -118.2437]).addTo(map);
 
-// Get data from cities.js
-let cityData = cities;
+// // Get data from cities.js
+// let cityData = cities;
 
 // // Loop through the cities array and create one marker for each city.
 // cityData.forEach(function(city) {
@@ -31,23 +31,23 @@ let cityData = cities;
 //     .addTo(map);
 //    });
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+// // Create the map object with a center and zoom level.
+// let map = L.map('mapid').setView([40.7, -94.5], 4);
 
-// Coordinates for each point to be used in the line.
-let line = [
-  [37.6213, -122.3790],
-  [30.18999924, -97.668663992],
-  [43.6775, -79.6308333333],
-  [40.6441666667, -73.7822222222]
-  ];
+// // Coordinates for each point to be used in the line.
+// let line = [
+//   [37.6213, -122.3790],
+//   [30.18999924, -97.668663992],
+//   [43.6775, -79.6308333333],
+//   [40.6441666667, -73.7822222222]
+//   ];
 
-// Create a polyline using the line coordinates and make the line red.
-L.polyline(line, {
-    color: "blue",
-    opacity: 0.5,
-    weight: 4
-  }).addTo(map);
+// // Create a polyline using the line coordinates and make the line red.
+// L.polyline(line, {
+//     color: "blue",
+//     opacity: 0.5,
+//     weight: 4
+//   }).addTo(map);
   
 // // Loop through the cities array and create one marker for each city.
 // cityData.forEach(function(city) {
@@ -68,8 +68,46 @@ L.polyline(line, {
 //     fillColor: '#ffffa1'
 // }).addTo(map);
 
+// Add GeoJSON data.
+let sanFranAirport =
+{"type":"FeatureCollection","features":[{
+    "type":"Feature",
+    "properties":{
+        "id":"3469",
+        "name":"San Francisco International Airport",
+        "city":"San Francisco",
+        "country":"United States",
+        "faa":"SFO",
+        "icao":"KSFO",
+        "alt":"13",
+        "tz-offset":"-8",
+        "dst":"A",
+        "tz":"America/Los_Angeles"},
+        "geometry":{
+            "type":"Point",
+            "coordinates":[-122.375,37.61899948120117]}}
+]};
+
+// // Grabbing our GeoJSON data.
+// L.geoJSON(sanFranAirport, {
+//     // We turn each feature into a marker on the map.
+//     pointToLayer: function(feature, latlng) {
+//       console.log(feature);
+//       return L.marker(latlng)
+//       .bindPopup("<h2>" + feature.properties.city + "</h2>")
+//     }
+//   }).addTo(map);
+
+// Grabbing our GeoJSON data.
+L.geoJson(sanFranAirport, {
+    onEachFeature: function(feature, layer) {
+        console.log(layer)
+        layer.bindPopup();
+     }
+});  
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -78,8 +116,6 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/til
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-// // We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 // mapbox/streets-v11
 // mapbox/outdoors-v11
 // mapbox/light-v10
